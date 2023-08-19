5d8db9569133487a3155cad2aa6bb0c55c6ec76c
Mustafa Melih Yumak <muyumak@k1m42s03.42istanbul.com.tr>
2023-08-19 08:52:11 +0300

    cpp

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -13,18 +13,85 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& source){
 	return *this;
 }
 
-char ScalarConverter::convertChar(std::string source){
+void ScalarConverter::convert(std::string arg){
+	if (ScalarConverter::is_char(arg) == true){
+
+		std::cout << "The arg is a char!" << std::endl;
+	}
+	else if (ScalarConverter::is_int(arg) == true)
+		std::cout << "The arg is an integer!" << std::endl;
+	else if (ScalarConverter::is_double(arg) == true)
+		std::cout << "The arg is a double!" << std::endl;
+	else if (ScalarConverter::is_float(arg) == true)
+		std::cout << "The arg is a float!" << std::endl;
+	else
+		std::cerr << "Invalid argument type!" << std::endl;
+
+	
+}
+
+bool ScalarConverter::is_char(std::string arg){
+	if (arg.size() != 1)
+		return false;
+	if (isdigit(arg[0]) != 0)
+		return false;
+	return true;
+}
+
+bool ScalarConverter::is_int(std::string arg){
+	for(int i = 0; i < arg.size(); i++){
+		if (isdigit(arg[i]) == 0)
+			return false;
+	}
+	return true;
+}
+
+bool ScalarConverter::is_float(std::string arg){
+	for(int i = 0; i < arg.size(); i++){
+		if (isdigit(arg[i]) == 0)
+			if (arg[i] != '.'){
+				if (arg[i] != 'f'){
+					return false;
+				}else if (i != arg.size() - 1)
+					return false;
+			}
+	}
+	int dotIndex = arg.find(".");
+	std::cout << "Dot index-> " << dotIndex << std::endl;
+	if (dotIndex == -1 || dotIndex == arg.size() - 1 || arg[arg.size() - 1] != 'f')
+		return false;
+	return true;
+}
+
+bool ScalarConverter::is_double(std::string arg){
+	for(int i = 0; i < arg.size(); i++){
+		if (isdigit(arg[i]) == 0)
+			if (arg[i] != '.')
+				return false;
+	}
+	int dotIndex = arg.find(".");
+	std::cout << "Dot index-> " << dotIndex << std::endl;
+	if (dotIndex == -1 || dotIndex == arg.size() - 1 || arg[arg.size() - 1] == 'f')
+		return false;
+	return true;
+}
+
+// Dönüşüm methodları yazılacak!
+
+char ScalarConverter::convertChar(std::string arg){
+	char result = arg[0];
+	return result;
+}
+
+int ScalarConverter::convertInt(std::string arg){
+	int result = std::stoi(arg);
+	return result;
+}
+
+float ScalarConverter::convertFloat(std::string arg){
 	return 0;
 }
 
-int ScalarConverter::convertInt(std::string source){
-	
-}
-
-float ScalarConverter::convertFloat(std::string source){
-	
-}
-
-double ScalarConverter::convertDouble(std::string source){
-	
+double ScalarConverter::convertDouble(std::string arg){
+	return 0;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -9,10 +9,18 @@ class ScalarConverter{
 		~ScalarConverter();
 		ScalarConverter(const ScalarConverter& source);
 		ScalarConverter& operator=(const ScalarConverter& source);
-		char convertChar(std::string source);
-		int convertInt(std::string source);
-		float convertFloat(std::string source);
-		double convertDouble(std::string source);
+
+		static void convert(std::string arg);
+
+		static bool is_char(std::string arg);
+		static bool is_int(std::string arg);
+		static bool is_float(std::string arg);
+		static bool is_double(std::string arg);
+
+		static char convertChar(std::string arg);
+		static int convertInt(std::string arg);
+		static float convertFloat(std::string arg);
+		static double convertDouble(std::string arg);
 };
 
 #endif
